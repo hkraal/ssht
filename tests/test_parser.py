d494355993fc6e62931c8ab363b38674c149899a
Henk Kraal <hkraal@users.noreply.github.com>
2022-10-27 17:15:50 +0200

    Import parsers dynamically

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -7,7 +7,6 @@ from ssht.plugins import Parser, Host, JsonParser
 
 
 class TestParser:
-
     def test_get_files(self, mocker):
         mocker.patch('os.listdir', return_value=['test.json', 'test.mysql'])
         parser = Parser('/tmp')
@@ -17,15 +16,16 @@ class TestParser:
 
     def test_search_hostname(self):
         parser = Parser('/tmp')
-        parser._hosts = [Host('host01.example.com'),
-                         Host('host02.example.com')]
+        parser._hosts = [Host('host01.example.com'), Host('host02.example.com')]
         assert parser.search('host01')[0].hostname == 'host01.example.com'
         assert len(parser.search('example.com')) == 2
 
     def test_search_ipv4(self):
         parser = Parser('/tmp')
-        parser._hosts = [Host('host01.example.com', ipv4='192.168.0.1'),
-                         Host('host02.example.com', ipv4='192.168.0.2')]
+        parser._hosts = [
+            Host('host01.example.com', ipv4='192.168.0.1'),
+            Host('host02.example.com', ipv4='192.168.0.2'),
+        ]
         assert parser.search('192.168.0.1')[0].hostname == 'host01.example.com'
         assert len(parser.search('192.168.0')) == 2
 
@@ -33,30 +33,25 @@ class TestParser:
         parser = Parser('/tmp')
         parser._hosts = [
             Host('host01.example.com', ipv6='fe80::41:dead:beef:cafe'),
-            Host('host02.example.com', ipv6='fe80::41:dead:beef:daff')]
-        assert parser.search('dead:beef:cafe')[
-                   0].hostname == 'host01.example.com'
+            Host('host02.example.com', ipv6='fe80::41:dead:beef:daff'),
+        ]
+        assert parser.search('dead:beef:cafe')[0].hostname == 'host01.example.com'
         assert len(parser.search('dead:beef:')) == 2
 
     def test_search_fnmatch_wildcard(self):
         parser = Parser('/tmp')
-        parser._hosts = [Host('host01.example.com'),
-                         Host('host02.example.com')]
-        assert parser.search('host*.example.com')[
-                   0].hostname == 'host01.example.com'
+        parser._hosts = [Host('host01.example.com'), Host('host02.example.com')]
+        assert parser.search('host*.example.com')[0].hostname == 'host01.example.com'
         assert len(parser.search('example.com')) == 2
 
     def test_search_fnmatch_questionmark(self):
         parser = Parser('/tmp')
-        parser._hosts = [Host('host01.example.com'),
-                         Host('host02.example.com')]
-        assert parser.search('host??.example.com')[
-                   0].hostname == 'host01.example.com'
+        parser._hosts = [Host('host01.example.com'), Host('host02.example.com')]
+        assert parser.search('host??.example.com')[0].hostname == 'host01.example.com'
         assert len(parser.search('host??.example.com')) == 2
 
 
 class TestJsonParser:
-
     def test_load_invalid_data(mocker):
         '''Ensure invalid JSON exceptions are catched properly'''
 
--- tests/test_ssht.py
+++ tests/test_ssht.py
@@ -12,85 +12,51 @@ from ssht.ssht import ssh_connect, select_host, get_log_level
 
 
 class TestConnect:
-
     def test_hostname(self, mocker):
         mocker.patch('subprocess.call')
-        ssh_connect(Host(hostname='host01.example.com'),
-                    [])
+        ssh_connect(Host(hostname='host01.example.com'), [])
         subprocess.call.assert_called_with(
             ['ssh', 'host01.example.com'],
         )
 
     def test_default_ipv4(self, mocker):
         mocker.patch('subprocess.call')
-        ssh_connect(Host(hostname='host01.example.com',
-                         ipv4='192.168.0.1'),
-                    [])
+        ssh_connect(Host(hostname='host01.example.com', ipv4='192.168.0.1'), [])
         subprocess.call.assert_called_with(
             ['ssh', '192.168.0.1'],
         )
 
     def test_port(self, mocker):
         mocker.patch('subprocess.call')
-        ssh_connect(
-            Host(
-                hostname='host01.example.com',
-                port='22022'),
-            [])
+        ssh_connect(Host(hostname='host01.example.com', port='22022'), [])
         subprocess.call.assert_called_with(
-            ['ssh',
-             'host01.example.com',
-             '-p',
-             '22022'],
+            ['ssh', 'host01.example.com', '-p', '22022'],
         )
 
     def test_ipv4_(self, mocker):
         mocker.patch('subprocess.call')
-        ssh_connect(
-            Host(
-                hostname='host01.example.com',
-                ipv4='192.168.1.2'),
-            ['-4'])
+        ssh_connect(Host(hostname='host01.example.com', ipv4='192.168.1.2'), ['-4'])
         subprocess.call.assert_called_with(
-            ['ssh',
-             '192.168.1.2',
-             '-4'],
+            ['ssh', '192.168.1.2', '-4'],
         )
 
     def test_ipv6(self, mocker):
         mocker.patch('subprocess.call')
-        ssh_connect(
-            Host(
-                hostname='host01.example.com',
-                ipv6='::1'),
-            ['-6'])
-        subprocess.call.assert_called_with(
-            ['ssh',
-             '::1',
-             '-6']
-        )
+        ssh_connect(Host(hostname='host01.example.com', ipv6='::1'), ['-6'])
+        subprocess.call.assert_called_with(['ssh', '::1', '-6'])
 
     def test_user(self, mocker):
         mocker.patch('subprocess.call')
-        ssh_connect(
-            Host(
-                hostname='host01.example.com',
-                user='admin'),
-            [])
-        subprocess.call.assert_called_with(
-            ['ssh',
-             'host01.example.com',
-             '-l',
-             'admin']
-        )
+        ssh_connect(Host(hostname='host01.example.com', user='admin'), [])
+        subprocess.call.assert_called_with(['ssh', 'host01.example.com', '-l', 'admin'])
 
 
-class TestSsht():
-
+class TestSsht:
     def test_select_host(self, mocker):
-        hosts = [Host(hostname='host01.example.com', ipv4='127.0.0.1'),
-                 Host(hostname='host02.example.com', ipv4='127.0.0.2',
-                      user='admin')]
+        hosts = [
+            Host(hostname='host01.example.com', ipv4='127.0.0.1'),
+            Host(hostname='host02.example.com', ipv4='127.0.0.2', user='admin'),
+        ]
 
         mocker.patch('ssht.ssht.get_answer', return_value='1')
         assert select_host(hosts).hostname == 'host01.example.com'
